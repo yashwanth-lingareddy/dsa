5e4ea4dd05c8c2049744d12d81de02f3ec0f2ccd
Yashwanth Lingareddy <yashwanth.hari@gmail.com>
2024-06-18 10:54:19 -0500

    kth largest element solution

--- techie-delight/306_kth_largest_element_in_array.py
+++ techie-delight/306_kth_largest_element_in_array.py
@@ -18,15 +18,31 @@ ToDo: this can also be done using min heap
 - Time Complexity: O(n log k)
 - Space Complexity: O(k) for the heap
 '''
-
+import heapq
 from typing import List
 
 def kth_largest_element(nums: List[int], k: int):
     nums.sort()
     return nums[len(nums) - k]
 
+def kth_latgest_element_using_heap(nums: List[int], k: int):
+    # create a heap with first k elements of the array
+    heap = [x for x in nums[:k]]
+    heapq.heapify(heap)
+
+    # iterate the remainin elements of the array, which is (n - k)
+    for n in nums[k:]:
+        # if the element is greatter than first item in the heap
+        # then remove the first item in the heap
+        # and insert this element to the heap
+        if n > heap[0]:
+            heapq.heappop(heap)
+            heapq.heappush(heap, n)
+    
+    return heap[0]
+
 if __name__=="__main__":
     nums = [1, 5, 2, 2, 2, 5, 5, 4]
     k = 4
-    ans = kth_largest_element(nums=nums, k=k)
+    ans = kth_latgest_element_using_heap(nums=nums, k=k)
     print(ans)
